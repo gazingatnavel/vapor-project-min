313121f829ceb25e5e3d0cce950826133c927b0a
gazingatnavel <gazingatnavel@users.noreply.github.com>
2018-05-06 16:27:48 -0400

    Add source files.
    Add Package.swift, Sources directory, and Tests directory.

---
+++ Sources/App/routes.swift
@@ -0,0 +1,13 @@
+import Routing
+import Vapor
+import Foundation
+
+/// Register your application's routes here.
+///
+/// [Learn More →](https://docs.vapor.codes/3.0/getting-started/structure/#routesswift)
+public func routes(_ router: Router) throws {
+    
+    router.get { req -> String in
+        return "Vapor Project Minimum\n"
+    }
+}
